fc498c9a2af9ad6388c44da195744705f8a73e3b
vipul sharma <129940441+vipul9045@users.noreply.github.com>
2023-04-06 12:38:42 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,14 @@
+import java.swap;
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your first string");
+        String up =  sc.next();
+        System.out.println("enter your second string");
+        String down = sc.next();
+        System.out.println("your strings are" + " " + up + down);
+
+    }
+    
+}
